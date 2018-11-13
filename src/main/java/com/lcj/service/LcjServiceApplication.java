0d114821f9a549e5ee262cea18943fd7f45394d2
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2018-11-13 22:25:19 +0900

    start SpringBoot WebService

---
+++ src/main/java/com/lcj/service/LcjServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lcj.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcjServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcjServiceApplication.class, args);
+	}
+	
+}
---
+++ src/main/java/com/lcj/service/controler/WebController.java
@@ -0,0 +1,14 @@
+package com.lcj.service.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+	
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+    
+}
