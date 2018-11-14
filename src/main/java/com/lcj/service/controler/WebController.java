12dd7105e26737e4e71f76601b61d355fd424d9f
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2018-11-15 00:16:24 +0900

    2018 11 14 jpa 적용

--- src/main/java/com/lcj/service/controler/WebController.java
+++ src/main/java/com/lcj/service/controler/WebController.java
@@ -1,14 +1,29 @@
 package com.lcj.service.controler;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lcj.service.domain.posts.PostsRepository;
+import com.lcj.service.dto.PostsSaveRequestDto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class WebController {
 	
+    private PostsRepository postsRepository;
+
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
     }
-    
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+    	System.out.println(dto.getTitle());
+        postsRepository.save(dto.toEntity());
+    }
 }
